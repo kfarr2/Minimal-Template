57e989f1a42188146874c5a03221e053256b67fd
konstantin <konstantin@iceking>
2015-01-03 00:13:34 -0800

    Upgrade to Django 1.7

---
+++ variables.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DB_ENGINE = 'django.db.backends.mysql'
+
+DB_USER = 'root'
+
+DB_PASSWORD = 'glasseye1'
+
+DB_NAME = 'mintemplate'
