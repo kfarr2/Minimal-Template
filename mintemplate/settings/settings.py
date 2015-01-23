8c37db7cb8c8d84798eff3a20bf795d454dd979b
Konstantin <kfarr2@pdx.edu>
2015-01-23 10:53:12 -0800

    Modify settings

--- mintemplate/settings/settings.py
+++ mintemplate/settings/settings.py
@@ -109,6 +109,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     here('../', "templates"),
+    ROOT("mintemplate/static"),
 )
 
 
