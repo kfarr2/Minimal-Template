154498fc9616880609735cd7c5dfc4a8c8759dc7
Konstantin <kfarr2@pdx.edu>
2015-01-23 19:08:59 -0800

    Add dev-banr

---
+++ mintemplate/static/dev-banr/banr.js
@@ -0,0 +1,13 @@
+/*
+ *  DEV-BANR
+ *  Written by Konstantin Farrell
+ *
+ *  Used for displaying a banner
+ *  reminding the user that they
+ *  are on a dev site.
+ *
+ *  Address all complaints to Sean
+ *
+ */
+
+var html = ''
