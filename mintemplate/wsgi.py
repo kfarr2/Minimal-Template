c10d1d2a2c1b2f7c9fccfea6dbf1272c46001a08
Konstantin <kfarr2@pdx.edu>
2015-01-28 19:47:43 -0800

    Edit the wsgi file

--- mintemplate/wsgi.py
+++ mintemplate/wsgi.py
@@ -8,7 +8,26 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mintemplate.settings")
+import sys
+import site
+
+prev_sys_path = list(sys.path)
+root = os.path.normpath(os.path.join(os.path.dirname(__file__), "../"))
+sys.path.append(root)
+site.addsitedir(os.path.join(root, ".env/lib/python%d.%d/site-packages" % sys.version_info[:2]))
+site.addsitedir(os.path.join(root, ".env/lib64/python%d.%d/site-packages" % sys.version_info[:2]))
+
+# addsitedir adds its directories at the end, but we want our local stuff
+# to take precedence over system-installed packages.
+# See http://code.google.com/p/modwsgi/issues/detail?id=112
+new_sys_path = []
+for item in list(sys.path):
+  if item not in prev_sys_path:
+    new_sys_path.append(item)
+    sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.path.basename(os.path.dirname(__file__)) + ".settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
